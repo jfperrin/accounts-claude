1cf37c1dddbb400185bfb8fdbee2cef91185eecb
jeff <jfperrin@artprice.com>
2026-04-09 16:18:35 +0200

    fix(client): remove double res.data unwrap in admin API client

--- client/src/api/admin.js
+++ client/src/api/admin.js
@@ -1,7 +1,7 @@
 import api from './client';
 
-export const getUsers      = ()              => api.get('/admin/users').then(r => r.data);
-export const createUser    = (data)          => api.post('/admin/users', data).then(r => r.data);
-export const updateUser    = (id, data)      => api.put(`/admin/users/${id}`, data).then(r => r.data);
-export const deleteUser    = (id)            => api.delete(`/admin/users/${id}`).then(r => r.data);
-export const sendReset     = (id)            => api.post(`/admin/users/${id}/reset-password`).then(r => r.data);
+export const getUsers   = ()         => api.get('/admin/users');
+export const createUser = (data)     => api.post('/admin/users', data);
+export const updateUser = (id, data) => api.put(`/admin/users/${id}`, data);
+export const deleteUser = (id)       => api.delete(`/admin/users/${id}`);
+export const sendReset  = (id)       => api.post(`/admin/users/${id}/reset-password`);
